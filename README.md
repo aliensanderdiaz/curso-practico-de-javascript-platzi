4200b7b828512dceac0457c20ba8824e609bba34
aliensanderdiaz <aliensanderdiaz@gmail.com>
2022-04-13 11:38:44 -0500

    reto: matematicas con javascript

--- README.md
+++ README.md
@@ -18,3 +18,49 @@
 ## Capítulo 6 de 24 - Calculando figuras geométricas en JavaScript
 
 ## Capítulo 7 de 24 - Encapsulando código en funciones
+
+## Capítulo 8 de 24 - Interactuando con JavaScript desde HTML
+
+## Capítulo 9 de 24 - Reto: matemáticas con JavaScript
+
+	En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+	function alturaTrianguloIsosceles(lado1, lado2, lado3) {
+
+		const esTrianguloIsosceles = lado1 === lado2 || lado1 === lado3 || lado2 === lado3
+		
+		if (!esTrianguloIsosceles) {
+			console.log({
+				mensaje: 'No es un triangulo isosceles, No puedo calcular la altura'
+			})
+			return
+		}
+
+		let ladoIgual
+		let base
+
+		if (lado1 === lado2) {
+			ladoIgual = lado1
+			base = lado3
+		}
+
+		if (lado1 === lado3) {
+			ladoIgual = lado1
+			base = lado2
+		}
+
+		if (lado2 === lado3) {
+			ladoIgual = lado2
+			base = lado1
+		}
+
+		const altura = Math.sqrt(Math.pow(ladoIgual, 2) - Math.pow((base / 2), 2))
+
+		console.log({
+			mensaje: 'Es un triángulo isosceles y la altura es ' + altura + 'cm'
+		})
+	}
+
+	alturaTrianguloIsosceles(1,2,3)
+	alturaTrianguloIsosceles(5,5,6)
+
--- figuras.js
+++ figuras.js
@@ -26,3 +26,56 @@ function perimetroCirculo(radio) {
 function areaCirculo(radio) {
     return Math.PI * Math.pow(radio, 2)
 }
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById('InputCuadrado')
+    const value = input.value
+    const perimetro = perimetroCuadrado(value)
+    alert(perimetro)
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById('InputCuadrado')
+    const value = input.value
+    const area = areaCuadrado(value)
+    alert(area)
+}
+
+function alturaTrianguloIsosceles(lado1, lado2, lado3) {
+
+    const esTrianguloIsosceles = lado1 === lado2 || lado1 === lado3 || lado2 === lado3
+    
+    if (!esTrianguloIsosceles) {
+        console.log({
+            mensaje: 'No es un triangulo isosceles, No puedo calcular la altura'
+        })
+        return
+    }
+
+    let ladoIgual
+    let base
+
+    if (lado1 === lado2) {
+        ladoIgual = lado1
+        base = lado3
+    }
+
+    if (lado1 === lado3) {
+        ladoIgual = lado1
+        base = lado2
+    }
+
+    if (lado2 === lado3) {
+        ladoIgual = lado2
+        base = lado1
+    }
+
+    const altura = Math.sqrt(Math.pow(ladoIgual, 2) - Math.pow((base / 2), 2))
+
+    console.log({
+        mensaje: 'Es un triángulo isosceles y la altura es ' + altura + 'cm'
+    })
+}
+
+alturaTrianguloIsosceles(1,2,3)
+alturaTrianguloIsosceles(5,5,6)
